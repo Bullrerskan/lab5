99f1124ab7b180a09be83e159cb89341776dd18f
Mebaselassie Kidane <76616663+mebakid74@users.noreply.github.com>
2021-03-09 15:13:22 +0100

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,19 +1,18 @@
 package general;
 
-
 import general.EventQueue;
 import general.Simulator;
 
 public abstract class Event {
 
-    private double time;
+    private double timeExcecution;
     
     public double getTime() {
-    	return time;
+    	return timeExcecution;
     }
    
     public void setTime(double time) { 
-    	this.time = time;
+    	this.timeExcecution = time;
     }
     
     public abstract void runEvent(Simulator state, EventQueue eventQueue);
