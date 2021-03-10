9b321d446505f7b5cad8df3f734eeea90d1c053d
Bullrerskan <69993258+Bullrerskan@users.noreply.github.com>
2021-03-10 22:23:29 +0100

    Update State.java

--- State.java
+++ State.java
@@ -20,7 +20,7 @@ public State(){
 }
 
 public boolean simStop(){ //returnerar simStop när det är dags att sluta.
-	return simStop();
+	return simStop(); // borde väl vara bara simStop; och inte simStop(); ? den med parenteser är metoden, medan den utan är bool-variabeln
 }
 
 public double timePassed() { // returnerar tiden som har gått.
